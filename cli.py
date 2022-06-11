d7f39656a1ad78a37e1450086fe4f7fbab2e591f
Kanan Bakirli <kanan.bk@gmail.com>
2022-06-11 08:36:28 -0400

    updated cli.py

--- cli.py
+++ cli.py
@@ -17,7 +17,7 @@ def mask(sentence):
     j = 1
     for a2 in answers2:
         click.echo(click.style((f"Prediction {j}:",
-                        list(a2.values())[0], "Score:", str(list(a2.values())[1])), fg="green"))
+                        list(a2.values())[2], "Score:", str(list(a2.values())[0])), fg="green"))
         j = j+1
 
 if __name__ == "__main__":
