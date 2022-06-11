2f5c0ec53611e7555d7dc0fdd2b11c6f4aac9947
Kanan Bakirli <kanan.bk@gmail.com>
2022-06-11 07:24:57 -0400

    updated test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from main import final_model
 
-def test_change():
+def test_us():
     assert list(final_model("The capital city of the United States is <mask>.")[0].values())[2] == ' Washington'
+
+def test_uk():
+    assert list(final_model("The capital city of the United Kingdom is <mask>.")[0].values())[2] == ' London'
     
     
