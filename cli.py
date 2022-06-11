a229a1710113214a008f7fe4657d64c4326e01e4
Kanan Bakirli <kanan.bk@gmail.com>
2022-06-11 07:17:41 -0400

    updating the CLI

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-import click 
+import click
 from main import final_model
 
 
@@ -8,19 +8,17 @@ from main import final_model
     prompt="Sentence",
     help="Takes a sentence with <mask> in place of any word and predicts that word!",
 )
-
-# the function for the CLI tool
-
 def mask(sentence):
+    # the function for the CLI tool
+
     """Example: Machine learning is an application of <mask>."""
-        
+
     answers2 = final_model(sentence)
-    
     j = 1
     for a2 in answers2:
         click.echo(click.style((f"Prediction {j}:",
                         list(a2.values())[0], "Score:", str(list(a2.values())[1])), fg="green"))
-        j = j+1    
+        j = j+1
 
 if __name__ == "__main__":
     mask()
